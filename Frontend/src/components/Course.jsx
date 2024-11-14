3d9dcef295e6045de82610f3bf49aeab555cdc34
Fatima_Iqra <iqraf683@gmail.com>
2024-11-14 13:02:36 +0500

    Initial commit

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="pt-28 items-center justify-center text-center">
+        <h1 className="text-2xl md:text-4xl  ">We're happy to <span className="text-pink-500">have you here! </span></h1>
+          <p className="mt-12">
+          Welcome to your one-stop bookstore, where every page holds a new adventure! Whether you're diving into health tips, savoring recipes, enjoying poetry, laughing at jokes, or getting lost in stories and literature, we have the perfect book waiting for you. Explore, discover, and get inspired!
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
